15fe3672e050c022ebf020e6c59ee41fb834734f
Victornfb <victornfb@outlook.com>
2023-03-07 22:10:47 -0300

    Feat: Add list empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,15 @@
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import ListEmpty from "@components/ListEmpty";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>([
-    "Grupo 🔑 dos Cornos",
-    "Novo ano velho 🎉",
+    // "Grupo 🔑 dos Cornos",
+    // "Novo ano velho 🎉",
   ]);
 
   return (
@@ -20,6 +21,10 @@ export default function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhum grupo encontrado" />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
     </Container>
   );
