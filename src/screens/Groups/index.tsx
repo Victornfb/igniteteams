e27a1744e197d64e9526d07eb464de161ebed49f
Victornfb <victornfb@outlook.com>
2023-03-07 22:01:44 -0300

    Feat: Add groups flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,26 @@
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Grupo 🔑 dos Cornos",
+    "Novo ano velho 🎉",
+  ]);
+
   return (
     <Container>
       <Header />
+      <Highlight title="Grupos" subtitle="Jogue com seu grupo de amigos!" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
